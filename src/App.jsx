42f9424e63d596eb461ef2bb92281a088df6f579
Divyanshu <divinnit@gmail.com>
2024-10-18 14:38:32 +0530

    my side of code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Content from './content'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Content/>
+    </>
+  )
+}
+
+export default App
